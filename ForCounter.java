6d6cf199b12e32ca27b1156d92f3c3d3504769cc
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-06 13:34:06 -0800

    Create ForCounter.java

---
+++ ForCounter.java
@@ -0,0 +1,15 @@
+//ForCounter.java
+//counter controlled repetition with the for repetition statement
+public class ForCounter
+{
+   public static void main(String[] args) 
+   {
+        
+      // loop increment
+      for (int counter = 1; counter <= 10; counter++) // for statement header includes initialization
+         System.out.printf("%d  ", counter);
+
+      System.out.println();
+   }
+} // end class 
+	
