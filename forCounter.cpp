6310f0ba16794a2cdd80f6dae0d4f7c931276356
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-06 13:35:14 -0800

    Create forCounter.cpp

---
+++ forCounter.cpp
@@ -0,0 +1,14 @@
+//forCounter.cpp
+//counter controlled repetition with the for repetition statement
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+		for (int counter = 1; counter <= 10; counter++)
+			cout<<"  "<<counter;
+
+		system("pause");
+	
+} // end class 
